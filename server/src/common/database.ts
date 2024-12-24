5791d01167f05a9edf0162a99f7baac08c9d9059
derickeroy2123 <derickeroy2123@gmail.com>
2024-12-24 14:28:15 +0800

    separated database connection to another file

---
+++ server/src/common/database.ts
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+import { DB_URI } from "./config";
+
+export async function connectToMongoDB() {
+    try {
+        await mongoose.connect(DB_URI);
+        console.log('Connected to MongoDB');
+    } catch (error) {
+        console.error(error);
+    }
+}
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,19 +1,13 @@
 import http from "http";
 import app from "./app";
-import mongoose from "mongoose";
-import { DB_URI } from "@common/config";
+import { connectToMongoDB } from "./common/database";
 
 (async () => {
-    try {
-        await mongoose.connect(DB_URI).then(() => console.log('Connected to MongoDB'));
+    await connectToMongoDB();
 
-        const server = http.createServer(app);
+    const server = http.createServer(app);
 
-        const port = 4000;
+    const port = 4000;
 
-        server.listen(port, () => console.log(`Server open at http://localhost:${port}`));
-    } catch (error) {
-        console.error(error);
-        process.exit(1);
-    }
+    server.listen(port, () => console.log(`Server open at http://localhost:${port}`));
 })();
