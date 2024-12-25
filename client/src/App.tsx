9843014907f96d767f071156fcad66b2db9524af
derickeroy2123 <derickeroy2123@gmail.com>
2024-12-25 16:34:19 +0800

    created basic router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,14 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import RegisterPage from "./pages/RegisterPage";
+import LoginPage from "./pages/LoginPage";
+
 export default function App() {
   return (
-    <div>App</div>
+    <BrowserRouter>
+        <Routes>
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/login" element={<LoginPage />} />
+        </Routes>
+    </BrowserRouter>
   )
 }
